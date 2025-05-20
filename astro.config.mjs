28c5e631410a6938d9cb9e8a6146127561670224
Hugo Martínez <hugov824@gmail.com>
2025-05-20 01:01:13 -0600

    Add Tailwind CSS configuration file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  base:'/hugocv/'
+  site: 'https://depresionTropical.github.io/hugocv/',
+  base: '/hugocv/',
+  integrations: [tailwind()],
 });
