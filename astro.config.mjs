bafe8d444469d9d35a569086ae3396b5f0b14cfd
Hugo Martínez <hugov824@gmail.com>
2025-05-21 20:41:14 -0600

    Refactor configuration and layout files for improved structure and Tailwind CSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://depresionTropical.github.io/hugocv/',
-  base: '/hugocv/',
-  integrations: [tailwind()],
+  base: '/',
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
